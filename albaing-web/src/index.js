381365e33a6c39387c4c5529c5ebebdc7cbd51c3
sin-doctor <sbs1602@naver.com>
2025-02-28 12:04:10 +0900

    jobpost

--- albaing-web/src/index.js
+++ albaing-web/src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './assets/styles/index.css';
-import DemoPage from "./pages/DemoPage";
+import Login2 from "./pages/login/Login2";
+import CompanyMain from "./pages/company/CompanyMain";
+import JobPostList from "./pages/jobpost/JobpostList";
+import JobPostDetail from "./pages/jobpost/JobpostDetail";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-      <DemoPage/>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Router>
+            <Routes>
+                <Route path="/login" element={<Login2 />} />
+                <Route path="/company" element={<CompanyMain />} />
+                <Route path="company/jobposts" element={<JobPostList />} />
+                <Route path="company/jobpost/:jobPostId" element={<JobPostDetail />} />
+            </Routes>
+        </Router>
+    </React.StrictMode>
 );
-
